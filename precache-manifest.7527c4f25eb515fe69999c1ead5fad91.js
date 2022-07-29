8005839745d74831d90251de84807fc11b1c76be
ketan <ketanfulzele2001@gmail.com>
2022-07-29 22:04:26 +0530

    Updates

---
+++ precache-manifest.7527c4f25eb515fe69999c1ead5fad91.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "dba9e9c494a15e4a6462",
+    "url": "/ketanfulzele/K10-friends.git/static/css/main.27d8b0fe.chunk.css"
+  },
+  {
+    "revision": "dba9e9c494a15e4a6462",
+    "url": "/ketanfulzele/K10-friends.git/static/js/main.dba9e9c4.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/ketanfulzele/K10-friends.git/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "858107d1ee4019ede1cf",
+    "url": "/ketanfulzele/K10-friends.git/static/css/2.30e81c51.chunk.css"
+  },
+  {
+    "revision": "858107d1ee4019ede1cf",
+    "url": "/ketanfulzele/K10-friends.git/static/js/2.858107d1.chunk.js"
+  },
+  {
+    "revision": "449fcc1dd9aafe7d516b",
+    "url": "/ketanfulzele/K10-friends.git/static/js/runtime~main.449fcc1d.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/ketanfulzele/K10-friends.git/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ketanfulzele/K10-friends.git/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "8df3ecd44b783adc1e8588fbd6b86f56",
+    "url": "/ketanfulzele/K10-friends.git/index.html"
+  }
+];
